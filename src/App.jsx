b05ba96a419b31fdc917fc48fc587e64b2dd642c
pjeong <pjeong@riotgames.com>
2022-11-19 02:04:21 +0900

    Test husky

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const temp1 = ''
 const App = () => {
-  const a = 'kkkkkk kkkkkkk'
+  const a = 'kkkkkk kkkkdkkk'
   const temp = () => `${a}1,2,3,dklklsjdfkafj al;kjfd;akdjf dk;fjadjfa;  da;lkjf;ajf  adskfja;fa sdsfdfs sdfsdf sdfsdf `
 
   return <>Test {temp()}</>
